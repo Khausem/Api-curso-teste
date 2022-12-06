4276ad4da01d29f203ca49b5062a310dbfdce917
Brenno Machado Correia <88114862+Khausem@users.noreply.github.com>
2022-12-05 22:27:15 -0300

    atualização
    versão 0.5

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
 //Requiring module
 const express = require('express')
+const mysql = require('mysql2');
+const connect = require('./conexao.js');
 
 //Creating express object
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.put('/clientes/:id',(req, res) =>{
+res.setHeader("Access-Control-Allow-Origin","*");
+res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+return connect.execSQLQuery("update cliente set nome='"+req.body.nome+"' where id="+req.params.id, res);
+})
 
 // Handling GET request 
 app.get('/', (req, res ) => { 
     res.send ('A api Está rodando ' + 'nesse servidor') 
     res.end () })
 
+    app.get('/clientes', (req, res) =>{
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        return connect.execSQLQuery('select * from cliente', res);
+    })
 
 // Handling GET request 
     app.get('/retorno', (req, res ) => { 
     res.send ('Nodejs é muito fácil') 
     res.end () })
 
+    app.get('/clientes/:id', (req, res) =>{
+        res.setHeader("Access-Control-Allow-Origin","*");
+        res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+
+return connect.execSQLQuery('select * from cliente where id='+ req.params.id, res);
+})
+
 // Port Number 
 const PORT = process.env.PORT || 5000;
 
 //ServerSetup
 app.listen(PORT,console.log(`Server started on port ${PORT}`))
+
+
+
+
+
+
+
+
+
+
+
+
